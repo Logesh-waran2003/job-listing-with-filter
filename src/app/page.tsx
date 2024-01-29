19f35ddc2aa58d53c841bb234e4d58f8004740a9
Logesh <Logeshwaransrini@gamil.com>
2024-01-30 02:45:37 +0530

    made mobile responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,44 @@
 import Image from "next/image";
 import React from "react";
 import bgHeaderDesktop from "@/assets/images/bg-header-desktop.svg";
+import bgHeaderMobile from "@/assets/images/bg-header-mobile.svg";
+import { IoCloseSharp } from "react-icons/io5";
+
 import JobCard from "@/components/JobCard";
+import data from "@/assets/data.json";
+
 const page = () => {
   return (
     <div className=" bg-[hsl(180,52%,96%)] min-h-screen w-full flex flex-col gap-12">
       <div className="w-full bg-[hsl(180,29%,50%)]">
-        <Image src={bgHeaderDesktop} alt="bgHeaderDesktop"></Image>
+        <Image
+          className="w-full md:hidden "
+          src={bgHeaderMobile}
+          alt="bg-header-mobile"
+        />
+        <Image
+          className="w-full hidden md:flex"
+          src={bgHeaderDesktop}
+          alt="bg-header-desktop"
+        />
       </div>
-      <main className="items-center w-full h-full flex flex-col">
-        <JobCard ></JobCard>
+      <main className="items-center w-full h-full flex flex-col gap-5 px-6 border mx-auto">
+        <div className="w-full bg-white shadow-lg py-4 px-6">
+          <button
+            // key={ind}
+            className="flex items-center"
+          >
+            <p className=" font-bold text-sm text-primary rounded px-2 py-0.5 bg-[hsl(180,52%,96%)] ">
+              Frontend
+            </p>
+            <div className=" bg-primary text-white hover:bg-slate-800">
+              <IoCloseSharp />
+            </div>
+          </button>
+        </div>
+        {data.map((data, ind) => (
+          <JobCard key={ind} {...data} />
+        ))}
       </main>
     </div>
   );
--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -2,6 +2,7 @@ import cn from "@/utils/cn";
 import Image from "next/image";
 import React from "react";
 import sampleImg from "@/assets/images/photosnap.svg";
+
 type Job = {
   id: number;
   company: string;
@@ -14,39 +15,76 @@ type Job = {
   postedAt: string;
   contract: string;
   location: string;
-  languages: string[];
-  tools: string[];
+  languages?: string[];
+  tools?: string[];
 };
 
-const JobCard = () => {
+const JobCard = (props: Job) => {
   return (
     <div
       className={cn(
-        "bg-white shadow-lg rounded flex justify-between w-full max-w-[950px] p-4 items-center border-l-4 border-primary"
+        "bg-white shadow-lg rounded md:flex justify-between w-full max-w-[950px] px-6 py-4 items-center relative mb-8",
+        { "border-l-4 border-primary": props.featured }
       )}
     >
       {/* left */}
-      <div className="flex gap-2">
-        <Image src={sampleImg} alt="sampleIng"></Image>
-        <div className="flex flex-col gap-2">
+      <div className="flex gap-4 ">
+        <Image
+          width={100}
+          height={100}
+          className="h-[70px] w-[70px] absolute md:static top-[-30px] left[-20px]"
+          src={props.logo}
+          alt="sampleIng"
+        ></Image>
+        <div className="flex flex-col gap-2 pt-8">
           <div className="flex items-center text-sm gap-2">
-            <p className="font-bold text-primary">Photosnap</p>
+            <p className="font-bold text-primary pr-4">{props.company}</p>
             <div className="flex items-center text-xs gap-2">
-              <div className=" bg-primary text-white font-bold rounded-full px-2 py-0.5">NEW</div>
-              <div className=" bg-slate-800 text-white font-bold rounded-full px-2 py-0.5">FEATURED</div>
+              {props.new && (
+                <div className=" bg-primary text-white font-bold rounded-full px-2 py-0.5">
+                  NEW!
+                </div>
+              )}
+              {props.featured && (
+                <div className=" bg-slate-800 text-white font-bold rounded-full px-2 py-0.5">
+                  FEATURED
+                </div>
+              )}
             </div>
           </div>
-          <p className=" font-bold">SDE</p>
+          <p className=" font-bold">{props.position}</p>
           <div className="flex text-xs text-gray-600 gap-6">
-            <p>1day ago</p>
-            <p>1day ago</p>
-            <p>1day ago</p>
+            <p>{props.postedAt}</p>
+            <p>{props.contract}</p>
+            <p>{props.location}</p>
           </div>
         </div>
       </div>
+      <hr className="md:hidden my-3 "/>
       {/* right side */}
-      <div className="">
-        <button className=" font-bold text-sm text-primary rounded px-2 py-0.5 bg-[hsl(180,52%,96%)]">HTML</button>
+      <div className="flex gap-3">
+        {props.languages &&
+          props.languages.map((languages, ind) => (
+            <div className="">
+              <button
+                key={ind}
+                className=" font-bold text-sm text-primary rounded px-2 py-0.5 bg-[hsl(180,52%,96%)]"
+              >
+                {languages}
+              </button>
+            </div>
+          ))}
+        {props.tools &&
+          props.tools.map((tools, ind) => (
+            <div className="">
+              <button
+                key={ind}
+                className=" font-bold text-sm text-primary rounded px-2 py-0.5 bg-[hsl(180,52%,96%)]"
+              >
+                {tools}
+              </button>
+            </div>
+          ))}
       </div>
     </div>
   );
